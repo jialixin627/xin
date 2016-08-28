a4a72cc8c42250252d71740fbc0e6a4265757dd4
jialixin <jialixin@aragoncs.com>
2016-08-29 00:20:50 +0800

    修改db为mysql

--- xin/xin/settings.py
+++ xin/xin/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'xin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'xin',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
